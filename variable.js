0683da44202ff2d1f3f1a596a19f482852625552
shefat26 <shefatmoral@gmail.com>
2022-11-22 14:16:21 -0500

    first commit

---
+++ variable.js
@@ -0,0 +1,76 @@
+console.log("shefat");
+
+console.log("moral");
+
+
+let name = 'Ovi';
+
+console.log(name);
+
+
+let firstName = 'Aveshek';        // this is a string 
+let lastName = 'Lashkar';
+
+console.log(firstName +""+lastName );
+ //console.log(lastName);
+
+
+ // CONSTANT  Variable ======
+
+// CONSTANT variable can only assigne value once, we can't change the value of CONSTANT variable.
+// If we need to change the value of a Constant, use some other keyword like let
+
+
+
+
+const age5 = 3;               // in CONSTANT or in Var we can declare one type of variable only once, like age.
+
+//interestRate = 4;
+
+const age1 = 4;
+//console.log(age );
+console.log(age1);
+
+
+//PRIMITIVE Dattype 
+
+
+
+let Name = 'Mosh';   //STRING  , this can be single quote or double quote
+
+let age = 30;        // This ia Number literal
+//let age =67;
+let isApproved = true       // Boolean Literal
+
+let firstName1 = undefined;      //Undefined  is a value also it is typeof  in Javascript.
+
+//let selectedColor = Null;     // Null is mean we have not set any valu here, we might or might not set any value in future.
+
+
+
+
+
+//Object  
+
+
+let person = {
+
+    userName: 'Mathew',
+    age: 45
+}
+
+
+
+
+person.userName = 'John';                 // we can change the above userName by using DOT Notation
+person.age = 33;
+
+
+//Bracket Notation  need to learn .
+
+
+console.log(person.age);
+
+//console.log(person.userName);
+
+
